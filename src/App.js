0de9dbb2ee1812eee4356287f25e557c3c356afd
AlexEMartin <alexeusebiomartin@gmail.com>
2022-06-10 21:07:06 -0300

    Uso de Fetch fakestore api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import ItemCount from './components/ItemCount';
+import ItemStore from './components/ItemStore';
 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <ItemStore />
       <ItemCount stock={5} />
     </div>
   );
---
+++ src/components/ItemStore.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react'
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;  
+    justify-content: space-around;
+    flex-wrap: wrap;
+    margin-top: 5rem;
+`;
+
+const Ficha = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`;
+
+const Image = styled.img`
+    width: 200px;
+    margin-left: 4rem;
+    margin-right: 4rem;
+`;
+
+const Desc = styled.p`
+    width: 200px;
+`;
+
+
+const ItemStore = () => {
+
+const [ productos, setProductos ] = useState([]);
+
+useEffect(() => {
+  obtenerDatos();
+
+}, [productos])
+
+
+const obtenerDatos = async() => {
+    const res = await fetch('https://fakestoreapi.com/products?limit=4'); 
+    const data = await res.json();
+    setProductos(data);
+}
+
+  return (
+    <Container>
+        {
+            productos.map(producto => {
+            return (
+                <Ficha key={producto.id}>
+                    <Image src={producto.image} alt='producto' />
+                    <Desc>{producto.title}</Desc>
+                    <Desc><strong>U$D {producto.price}</strong></Desc>
+                </Ficha>
+            )})
+        }
+    </Container>
+  )
+}
+
+export default ItemStore
